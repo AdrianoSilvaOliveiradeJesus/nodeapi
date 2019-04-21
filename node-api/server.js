74e4c5ac4322e2b2115f0f83550da2240176ee12
Adriano Silva <adrianosilvaoliveiradejesus@gmail.com>
2019-04-20 21:49:44 -0300

    Initial Commit

---
+++ node-api/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
+
+//Iniciando o App
+const app = express();
+
+//Iniciando o DB
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi',{useNewUrlParser:true}
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// Primeira rota
+app.get('/',(req,res) =>{
+  Product.create({
+    title:"React Native",
+    description:"Build native apps with react",
+    url:"http://google.com"
+  })
+  return res.send('Hello,World !');
+});
+
+app.listen(3001);
